ab90e7ed5befa668e99ef1bc9c2b48da75e2acab
kawrae <coreyblack@hotmail.co.uk>
2024-09-26 17:35:04 +0100

    Changed wave properties

--- script.js
+++ script.js
@@ -12,10 +12,10 @@ canvas2.width = width;
 canvas2.height = height;
 
 // Wave properties
-const waveSpeed = 0.00045;
+const waveSpeed = 0.00080;
 const waveAmplitude = 50;
-const waveFrequency1 = 0.0065;
-const waveFrequency2 = 0.007;
+const waveFrequency1 = 0.0045;
+const waveFrequency2 = 0.0050;
 
 // Particle properties
 const particleCount = 10;
@@ -27,14 +27,24 @@ const maxLifetime = 400; // Maximum lifetime in milliseconds
 // Particles array to store their positions, lifetimes, and alpha values
 const particles = [];
 
+// Create gradients for the waves
+const gradient1 = ctx1.createLinearGradient(0, height / 2, 0, height);
+gradient1.addColorStop(0, 'rgba(1, 28, 148, 0.3)');  // Top color: rgb(1, 28, 148)
+gradient1.addColorStop(1, 'rgba(0, 5, 54, 0.5)');   // Bottom color: rgb(0, 5, 54)
+
+const gradient2 = ctx2.createLinearGradient(0, height / 2, 0, height);
+gradient2.addColorStop(0, 'rgba(1, 28, 148, 0.3)');  // Top color with transparency: rgb(1, 28, 148)
+gradient2.addColorStop(1, 'rgba(0, 5, 54, 0)');   // Bottom color with full transparency: rgb(0, 5, 54)
+
+
 // Animation loop
 function animate() {
   requestAnimationFrame(animate);
   ctx1.clearRect(0, 0, width, height);
   ctx2.clearRect(0, 0, width, height);
 
-  drawWave(ctx1, waveFrequency1, 'rgba(255, 255, 255, 0.05)', performance.now()); /* White with transparency */
-  drawWave(ctx2, waveFrequency2, 'rgba(255, 255, 255, 0.1)', performance.now()); /* White with transparency */
+  drawWave(ctx1, waveFrequency1, gradient1, performance.now()); /* Gradient with transparency */
+  drawWave(ctx2, waveFrequency2, gradient2, performance.now()); /* Gradient with transparency */
 
   updateParticles();
   drawParticles(ctx1);
@@ -42,7 +52,7 @@ function animate() {
 }
 
 // Draw waves
-function drawWave(ctx, frequency, color, timestamp) {
+function drawWave(ctx, frequency, gradient, timestamp) {
   const opacity = Math.min((timestamp / 1000) * 1, 1); // Adjust the multiplier for faster fading
   ctx.globalAlpha = opacity; // Set global alpha for the wave
 
@@ -55,8 +65,8 @@ function drawWave(ctx, frequency, color, timestamp) {
   ctx.lineTo(width, height);
   ctx.closePath();
 
-  // Fill wave with color
-  ctx.fillStyle = color;
+  // Fill wave with gradient color for smooth transitions
+  ctx.fillStyle = gradient;
   ctx.fill();
 
   ctx.globalAlpha = 1; // Reset global alpha
